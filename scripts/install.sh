d83f228b9c86a95c97d482d907c141c2138f0615
avsbestua <rmetrikovskij@gmail.com>
2026-03-18 22:33:53 +0200

    macos and linux install script fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,9 +3,11 @@
 curl -o iprad.zip -L https://github.com/avsbestua/iprad/archive/refs/heads/main.zip
 unzip iprad.zip
 
-cd iprad
+cd iprad-main
 
 pip3 install -e .
 
+clear
+rm iprad.zip
 echo "Install finished. Press enter to exit"
 read
