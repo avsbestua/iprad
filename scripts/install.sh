3e5bb2d047ce614fc9b1caeb5aaa505f93d922c5
avsbestua <rmetrikovskij@gmail.com>
2026-03-19 19:23:50 +0200

    macos and linux install script rework part 1

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
 
-curl -o iprad.zip -L https://github.com/avsbestua/iprad/archive/refs/heads/main.zip
-unzip iprad.zip
+curl -L -o iprad.zip https://github.com/avsbestua/iprad/archive/refs/heads/main.zip
+
+
+HASH_FILE="$HOME/iprad.hash"
+
+
+if command -v sha256sum >/dev/null; then
+    NEW_HASH=$(sha256sum iprad.zip | awk '{ print $1 }')
+else
+    NEW_HASH=$(shasum -a 256 iprad.zip | awk '{ print $1 }')
+fi
+
+
+if [ -f "$HASH_FILE" ]; then
+    OLD_HASH=$(cat "$HASH_FILE")
+
+ 
+    if [ "$NEW_HASH" == "$OLD_HASH" ]; then
+        echo "You have the latest version"
+        rm iprad.zip
+        exit 0
+    fi
+fi
+
+
+echo "$NEW_HASH" > "$HASH_FILE"
+
+
+unzip iprad.zip 
+
 
 cd iprad-main
 
-pip3 install -e .
+
+pip install -e .
+
+
+cd ..
+
 
 clear
 rm iprad.zip
